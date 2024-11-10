95cd0da617e6b3a32aa2f323e8c6082544b7e742
Emma <Emma@fanap.ir>
2024-11-10 17:31:03 +0330

    inital project and add prettier and husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      "prettier",
-    ],
-    files: ["**/*.{ts,tsx}"],
+    ignores: ['dist'],
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
+      parser: tsParser,
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
     },
     rules: {
+      ...js.configs.recommended.rules, // If you want to include ESLint's recommended rules
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
+      ...tsPlugin.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
     },
-  }
-);
+  },
+];
