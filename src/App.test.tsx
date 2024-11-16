5ac5b3a82ef0695f1233a3e894c7813507f021d8
Emma <Emma@fanap.ir>
2024-11-16 15:59:27 +0330

    add another test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
-import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { afterEach, describe, expect, test } from "vitest";
+import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 
 import App from "./App";
+afterEach(cleanup);
 
 describe("App", () => {
   test("Should render input field and add button", () => {
@@ -14,11 +15,17 @@ describe("App", () => {
     expect(button).toBeInTheDocument();
   });
 
-  // test("should clear the input field after adding a task", async () => {
-  //   render(<App />);
-  //   const input = screen.getByRole("textbox", { name: "Add Task:" });
-  //   const button = screen.getByRole("button", { name: "Add" });
-  // });
+  test("should clear the input field after adding a task", () => {
+    render(<App />);
+
+    const input = screen.getByRole("textbox", { name: "Add Task:" });
+    const button = screen.getByRole("button", { name: "Add" });
+
+    fireEvent.change(input, { target: { value: "Test New Task" } });
+    fireEvent.click(button);
+
+    expect(screen.getByText("Test New Task")).toBeInTheDocument();
+  });
   test("should not add an empty task", () => {});
   test("should add a task by pressing the enter key", () => {});
 });
